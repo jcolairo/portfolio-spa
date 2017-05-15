2f18c6f9868a6faa4254b36679879c5435ca22ed
James Colairo <jcolairo@spartaglobal.co>
2017-05-15 16:48:13 +0100

    linking page on repeating projects

--- js/controllers/portfolio.controller.js
+++ js/controllers/portfolio.controller.js
@@ -4,24 +4,24 @@ function PortfolioController($state) {
   controller.portfolioData = [
     {
       title: 'Connect 4',
+      titleName: 'portfolio-connect4',
       img: '',
       desc: '',
-      link: 'https://jcolairo-sg-project-1.herokuapp.com/',
-      linkName: 'Connect 4'
+      link: 'https://jcolairo-sg-project-1.herokuapp.com/'
     },
     {
       title: 'Find your favourite Movie',
+      titleName: 'portfolio-movies',
       img: '',
       desc: '',
-      link: 'https://users-movie-mvc.herokuapp.com/',
-      linkName: 'User, Movies'
+      link: 'https://users-movie-mvc.herokuapp.com/'
     },
     {
       title: 'Find crime in your area',
+      titleName: 'portfolio-police',
       img: '',
       desc: '',
-      link: 'https://james-sg-project-3.herokuapp.com/',
-      linkName: 'Crime'
+      link: 'https://james-sg-project-3.herokuapp.com/'
     }
   ];
 }
