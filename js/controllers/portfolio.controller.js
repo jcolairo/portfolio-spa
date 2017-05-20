24306ba7af75364fdaa6946089c442dbdfe900d1
James Colairo <jcolairo@spartaglobal.co>
2017-05-20 15:24:27 +0100

    added description for projects

--- js/controllers/portfolio.controller.js
+++ js/controllers/portfolio.controller.js
@@ -13,14 +13,14 @@ function PortfolioController() {
       title: 'Find your favourite Movie',
       titleName: 'portfolio-movies',
       img: 'movies.png',
-      desc: 'movies users desciption',
+      desc: 'movies users desciption Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       link: 'https://users-movie-mvc.herokuapp.com/'
     },
     {
       title: 'Find crime in your area',
       titleName: 'portfolio-police',
       img: 'police.png',
-      desc: 'police api desciption',
+      desc: 'police api desciption Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       link: 'https://james-sg-project-3.herokuapp.com/'
     }
   ];
