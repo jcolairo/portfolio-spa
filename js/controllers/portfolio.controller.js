8188ed9883b96a86ae56dc446ff0093c6db77c5b
James Colairo <jcolairo@spartaglobal.co>
2017-07-22 16:45:37 +0100

    added short description in controller

--- js/controllers/portfolio.controller.js
+++ js/controllers/portfolio.controller.js
@@ -7,6 +7,7 @@ function PortfolioController() {
       titleName: 'portfolio-connect4',
       img: 'connect4',
       desc: 'Connect 4 desciption Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      shortDesc: 'short desciption',
       link: 'https://jcolairo-sg-project-1.herokuapp.com/'
     },
     {
@@ -14,6 +15,7 @@ function PortfolioController() {
       titleName: 'portfolio-movies',
       img: 'movies.png',
       desc: 'movies users desciption Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      shortDesc: 'short desciption',
       link: 'https://users-movie-mvc.herokuapp.com/'
     },
     {
@@ -21,6 +23,7 @@ function PortfolioController() {
       titleName: 'portfolio-police',
       img: 'police.png',
       desc: 'police api desciption Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      shortDesc: 'short desciption',
       link: 'https://james-sg-project-3.herokuapp.com/'
     }
   ];
