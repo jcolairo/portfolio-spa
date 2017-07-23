5d2c120aafb87899a0b9e1390d5f6e13d43cd402
James Colairo <jcolairo@spartaglobal.co>
2017-07-23 12:54:12 +0100

    added polariod name

--- js/controllers/portfolio.controller.js
+++ js/controllers/portfolio.controller.js
@@ -5,6 +5,7 @@ function PortfolioController() {
     {
       title: 'Connect 4',
       titleName: 'portfolio-connect4',
+      polariodName: 'Connect 4',
       img: 'connect4',
       desc: 'Connect 4 desciption Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       shortDesc: 'short desciption',
@@ -13,6 +14,7 @@ function PortfolioController() {
     {
       title: 'Find your favourite Movie',
       titleName: 'portfolio-movies',
+      polariodName: 'Movies',
       img: 'movies.png',
       desc: 'movies users desciption Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       shortDesc: 'short desciption',
@@ -21,6 +23,7 @@ function PortfolioController() {
     {
       title: 'Find crime in your area',
       titleName: 'portfolio-police',
+      polariodName: 'Crime API',
       img: 'police.png',
       desc: 'police api desciption Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       shortDesc: 'short desciption',
