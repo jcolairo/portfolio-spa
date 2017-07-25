62bc146e6a611ce67922ec8298a1534d0bd3be6d
James Colairo <jcolairo@spartaglobal.co>
2017-07-25 13:15:20 +0100

    changed order of projects

--- js/controllers/portfolio.controller.js
+++ js/controllers/portfolio.controller.js
@@ -12,15 +12,6 @@ function PortfolioController() {
       link: 'https://jcolairo-sg-project-1.herokuapp.com/'
     },
     {
-      title: 'Find your favourite Movie',
-      titleName: 'portfolio-movies',
-      polariodName: 'Movies',
-      img: 'movies.png',
-      desc: 'movies users desciption Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      shortDesc: 'short desciption',
-      link: 'https://users-movie-mvc.herokuapp.com/'
-    },
-    {
       title: 'Find crime in your area',
       titleName: 'portfolio-police',
       polariodName: 'Crime API',
@@ -28,6 +19,15 @@ function PortfolioController() {
       desc: 'police api desciption Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       shortDesc: 'short desciption',
       link: 'https://james-sg-project-3.herokuapp.com/'
+    },
+    {
+      title: 'Find your favourite Movie',
+      titleName: 'portfolio-movies',
+      polariodName: 'Movies',
+      img: 'movies.png',
+      desc: 'movies users desciption Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+      shortDesc: 'short desciption',
+      link: 'https://users-movie-mvc.herokuapp.com/'
     }
   ];
 
