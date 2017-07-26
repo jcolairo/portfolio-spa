75821c49c0332532fd72146bd912dc43bf41fe46
James Colairo <jcolairo@spartaglobal.co>
2017-07-26 20:36:21 +0100

    added linkedIn link

--- js/controllers/portfolio.controller.js
+++ js/controllers/portfolio.controller.js
@@ -38,7 +38,8 @@ function PortfolioController() {
     {
       desc: 'About me Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       email: 'jamescolairo37@gmail.com',
-      githubLink: 'https://github.com/jcolairo'
+      githubLink: 'https://github.com/jcolairo',
+      linkedin: 'https://www.linkedin.com/in/james-colairo/'
     }
   ];
 
