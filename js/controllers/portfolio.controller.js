963f56f8ea1bde8e89fba2728b57ab1e93592dc9
James Colairo <jcolairo@spartaglobal.co>
2017-07-27 18:19:35 +0100

    added skills to controller

--- js/controllers/portfolio.controller.js
+++ js/controllers/portfolio.controller.js
@@ -39,7 +39,20 @@ function PortfolioController() {
       desc: 'About me Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
       email: 'jamescolairo37@gmail.com',
       githubLink: 'https://github.com/jcolairo',
-      linkedin: 'https://www.linkedin.com/in/james-colairo/'
+      linkedin: 'https://www.linkedin.com/in/james-colairo/',
+      skills: [
+        'jQuery',
+        'CSS3',
+        'SCSS',
+        'HTML 5',
+        'JS',
+        'Angular JS',
+        'Node.js',
+        'Express.js',
+        'Github',
+        'MongoDB',
+        'Firebase'
+      ]
     }
   ];
 
